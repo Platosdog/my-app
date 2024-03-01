9d11da2913ae1c6c206a9b5f59320bd8e13db95e
Platosdog <62915572+Platosdog@users.noreply.github.com>
2024-03-01 09:10:16 +0900

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyComponentComponent } from './my-component/my-component.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponentComponent,
+    ContactFormComponent
   ],
   imports: [
     BrowserModule,
